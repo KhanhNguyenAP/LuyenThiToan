92991322e0e66c468622553045cea5548b02cdad
Khánh Nguyễn <Khnh Nguyn>
2016-06-10 23:20:39 +0700

    download data first app

--- app/src/main/java/com/thud/huynhnhu/luyenthitoan/activities/SplashActivity.java
+++ app/src/main/java/com/thud/huynhnhu/luyenthitoan/activities/SplashActivity.java
@@ -9,8 +9,15 @@ import android.preference.Preference;
 import android.test.SingleLaunchActivityTestCase;
 
 import com.thud.huynhnhu.luyenthitoan.R;
+import com.thud.huynhnhu.luyenthitoan.adapter.ContentAdapter;
+import com.thud.huynhnhu.luyenthitoan.datas.ContentDAL;
+import com.thud.huynhnhu.luyenthitoan.model.Content;
+import com.thud.huynhnhu.luyenthitoan.model.Result;
+import com.thud.huynhnhu.luyenthitoan.model.ResultStatus;
 import com.thud.huynhnhu.luyenthitoan.utils.async.SaveAllDataFromSerVer;
 import com.thud.huynhnhu.luyenthitoan.utils.interfaces.Flags;
+
+import java.util.ArrayList;
 
 /**
  * Created by KhanhNguyen on 5/11/2016.
@@ -18,6 +25,7 @@ import com.thud.huynhnhu.luyenthitoan.utils.interfaces.Flags;
  */
 public class SplashActivity extends Activity{
     private Preference preference;
+    public static ArrayList<Content> arr_Content = new ArrayList<Content>();
 
     // Splash screen timer
     private static int SPLASH_TIME_OUT = 3000;
@@ -34,6 +42,8 @@ public class SplashActivity extends Activity{
             new SaveAllDataFromSerVer(SplashActivity.this).executeOnExecutor(AsyncTask.THREAD_POOL_EXECUTOR);
         }
 
+        new apiGetContent().executeOnExecutor(AsyncTask.THREAD_POOL_EXECUTOR);
+
         new Handler().postDelayed(new Runnable() {
             @Override
             public void run() {
@@ -44,5 +54,29 @@ public class SplashActivity extends Activity{
                 finish();
             }
         }, SPLASH_TIME_OUT);
+    }
+
+    private class apiGetContent extends AsyncTask<String, Void, Result<ArrayList<Content>>>{
+        @Override
+        protected void onPreExecute(){
+            super.onPreExecute();
+        }
+
+        @Override
+        protected Result<ArrayList<Content>> doInBackground(String... strings){
+            return new ContentDAL(SplashActivity.this).getAllConTentFromLoCal(Flags.chosen_ma_kienthuc);
+        }
+
+        @Override
+        protected void onPostExecute(Result<ArrayList<Content>> arrayListResult){
+            super.onPostExecute(arrayListResult);
+            if (arrayListResult.getKey() == ResultStatus.TRUE){
+                arr_Content = arrayListResult.getValue();
+            }
+
+            if (arr_Content.size() == 0){
+                new SaveAllDataFromSerVer(SplashActivity.this).executeOnExecutor(AsyncTask.THREAD_POOL_EXECUTOR);
+            }
+        }
     }
 }
